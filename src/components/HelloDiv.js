fc9de0e6d12a5c6a4512daf6674c2d1c727939c8
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-06-12 19:51:28 -0400

    Added HTML to HelloDiv function

--- src/components/HelloDiv.js
+++ src/components/HelloDiv.js
@@ -1,3 +1,14 @@
-const HelloDiv = () => null;
+import React from "react";
+
+const HelloDiv = () => (
+    <div>
+        <h1>Hello, my name is Joe</h1>
+        <ul>Things I like
+            <li>guacamole</li>
+            <li>sushi</li>
+            <li>pasta</li>
+        </ul>
+    </div>
+);
 
 export default HelloDiv;
