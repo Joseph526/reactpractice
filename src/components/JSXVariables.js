5b38449c0cc1e7a221f38f2be2cd9a00df984a67
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-06-12 20:58:18 -0400

    Tried to add noVowels function

--- src/components/JSXVariables.js
+++ src/components/JSXVariables.js
@@ -4,6 +4,18 @@ const name = "Joseph";
 const numLetters = 6;
 const reactThoughts = "is pretty cool so far.";
 
+const noVowels = (name) => {
+  const vowels = ["a", "e", "i", "o", "u"];
+  let result = "";
+
+  for (let i = 0; i < name.length; i++) {
+    if (!(name[i] in vowels)) {
+      result += name[i];
+    }
+  }
+  return result;
+};
+
 const JSXVariables = () => (
   <div className="main-container">
     <div className="container">
@@ -11,6 +23,7 @@ const JSXVariables = () => (
         <h1>Hi! My name is {name}</h1>
         <h2>My name has {numLetters} letters</h2>
         <h2>I think React {reactThoughts}</h2>
+        <h2>My name without vowels is {noVowels(name)}</h2>
       </div>
     </div>
   </div>
