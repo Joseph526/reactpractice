96db7535663c0766a07367b08f54e67952489f36
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-06-12 20:45:45 -0400

    Used JSX variables

--- src/components/JSXVariables.js
+++ src/components/JSXVariables.js
@@ -1,12 +1,16 @@
 import React from "react";
 
+const name = "Joseph";
+const numLetters = 6;
+const reactThoughts = "is pretty cool so far.";
+
 const JSXVariables = () => (
   <div className="main-container">
     <div className="container">
       <div className="jumbotron">
-        <h1>Hi! My name is (insert name here)</h1>
-        <h2>My name has (insert number of letters in name here) letters</h2>
-        <h2>I think React (insert thoughts about React here)</h2>
+        <h1>Hi! My name is {name}</h1>
+        <h2>My name has {numLetters} letters</h2>
+        <h2>I think React {reactThoughts}</h2>
       </div>
     </div>
   </div>
