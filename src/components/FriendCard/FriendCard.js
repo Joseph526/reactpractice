67e39193f51f3357930d4302ddc277efa5b93cd5
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-06-14 19:55:26 -0400

    Changed to use props

--- src/components/FriendCard/FriendCard.js
+++ src/components/FriendCard/FriendCard.js
@@ -1,24 +1,24 @@
 import React from "react";
 import "./FriendCard.css";
 
-const FriendCard = () => (
+const FriendCard = (props) => (
   <div className="card">
     <div className="img-container">
       <img
-        alt="SpongeBob"
-        src="https://upload.wikimedia.org/wikipedia/en/thumb/4/47/Spongebob-squarepants.svg/666px-Spongebob-squarepants.svg.png"
+        alt={props.name}
+        src={props.image}
       />
     </div>
     <div className="content">
       <ul>
         <li>
-          <strong>Name:</strong> SpongeBob
+          <strong>Name:</strong> {props.name}
         </li>
         <li>
-          <strong>Occupation:</strong> Fry Cook
+          <strong>Occupation:</strong> {props.occupation}
         </li>
         <li>
-          <strong>Location:</strong> A Pinapple Under the Sea
+          <strong>Location:</strong> {props.location}
         </li>
       </ul>
     </div>
