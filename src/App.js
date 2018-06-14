b4262bd3af30ba312acf19ed8d2f65140d6f0412
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-06-14 19:50:57 -0400

    Created generic FriendCard component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
+import FriendCard from "./components/FriendCard";
 import friends from "./friends.json";
 
 const App = () => (
   <Wrapper>
     <Title>Friends List</Title>
+      <FriendCard/>
   </Wrapper>
 );
 
