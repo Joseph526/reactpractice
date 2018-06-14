86d69357ca4375b16ce052de2478d32f0ccb1703
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-06-14 19:44:22 -0400

    Removed redundant Card JSX

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from "react";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
-import SpongeBobCard from "./components/SpongeBobCard";
-import SquidwardCard from "./components/SquidwardCard";
-import MrKrabsCard from "./components/MrKrabsCard";
+import friends from "./friends.json";
 
 const App = () => (
   <Wrapper>
     <Title>Friends List</Title>
-    <SpongeBobCard />
-    <MrKrabsCard />
-    <SquidwardCard />
   </Wrapper>
 );
 
