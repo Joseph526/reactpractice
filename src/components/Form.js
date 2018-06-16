f06af3bd118649b452ce3560bd80e9dc82b9117d
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-06-16 10:26:21 -0400

    Added form event handlers

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,20 +7,59 @@ class Form extends Component {
     password: ""
   };
 
+  handleInputChange = event => {
+    // Getting the value and name of the input which triggered the change
+    let value = event.target.value;
+    const name = event.target.name;
+
+    if (name === "password") {
+      value = value.substring(0, 15);
+    }
+    // Updating the input's state
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    event.preventDefault();
+    if (!this.state.username) {
+      alert("Fill out your username please!");
+    } else if (this.state.password.length < 6) {
+      alert(
+        `Choose a more secure password ${this.state.username}`
+      );
+    } else {
+      alert(`Username: ${this.state.username} \nPassword: ${this.state.password}`);
+    }
+
+    this.setState({
+      username: "",
+      password: ""
+    });
+  };
+
   render() {
     return (
       <form>
         <p>Username: {this.state.username}</p>
         <p>Password: {this.state.password}</p>
         <input
+          value={this.state.username}
+          name="username"
+          onChange={this.handleInputChange}
           type="text"
           placeholder="Username"
         />
         <input
+          value={this.state.password}
+          name="password"
+          onChange={this.handleInputChange}
           type="password"
           placeholder="Password"
         />
-        <button>Submit</button>
+        <button onClick={this.handleFormSubmit}>Submit</button>
       </form>
     );
   }
