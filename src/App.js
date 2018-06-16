1b85ba8876c932d538f960768d28b28333017cd4
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-06-16 10:09:38 -0400

    Replaced src directory

--- src/App.js
+++ src/App.js
@@ -1,21 +1,6 @@
 import React from "react";
-import FriendCard from "./components/FriendCard";
-import Wrapper from "./components/Wrapper";
-import friends from "./friends.json";
-import "./App.css";
+import Form from "./components/Form";
 
-const App = () => (
-  <Wrapper>
-    <h1 className="title">Friends List</h1>
-    {friends.map(friendItem => (
-      <FriendCard
-        name={friendItem.name}
-        image={friendItem.image}
-        occupation={friendItem.occupation}
-        location={friendItem.location}
-      />
-    ))};
-  </Wrapper>
-);
+const App = () => <Form />;
 
 export default App;
